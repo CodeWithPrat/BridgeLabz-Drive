69be1e57b863b21ea8129c271d24f165b06a7b6d
PRATIK <139374406+CodeWithPrat@users.noreply.github.com>
2024-01-08 23:00:39 +0530

    Create primeNumber.java

---
+++ Logical Programming/Prime Number/primeNumber.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PrimeNumbersInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting number of the range: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number of the range: ");
+        int end = scanner.nextInt();
+
+        System.out.println("Prime numbers within the range " + start + " to " + end + ":");
+        findAndPrintPrimes(start, end);
+
+        scanner.close();
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to find and print prime numbers within a range
+    public static void findAndPrintPrimes(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); // Print a new line after the prime numbers
+    }
+}
